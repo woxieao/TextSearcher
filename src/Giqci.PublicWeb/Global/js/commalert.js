93f16fda0b48cecbeca38118c337a4f3a9c58c41
LiuXiang <liuxiang@huiber.cn>
2015-12-05 17:53:47 +0800

    菜单调整为中文
    部分页面ui调整，优化
    alert提示信息逻辑优化
    页面适应移动端

---
+++ src/Giqci.PublicWeb/Global/js/commalert.js
@@ -0,0 +1,27 @@
+﻿angular.module('commonAlert', ['ng']).value("alerts", []).factory('commAlertService', ['$rootScope', '$timeout', 'alerts', function ($rootScope, $timeout, alerts) {
+    return {
+        "alertService": function () {
+            var alertJson = {};
+            $rootScope.alerts = alerts;
+            alertJson.add = function (type, msg, time) {
+                for (var x = 1 in $rootScope.alerts) {
+                    $rootScope.alerts.splice(x, 1);
+                }
+                $rootScope.alerts.push({
+                    'type': type, 'msg': msg, 'close': function () {
+                        alertJson.closeAlert(this);
+                    }
+                });
+                if (time) {
+                    $timeout(function () {
+                        $rootScope.alerts = [];
+                    }, time);
+                }
+            };
+            alertJson.closeAlert = function (alert) {
+                $rootScope.alerts.splice($rootScope.alerts.indexOf(alert), 1);
+            };
+            return alertJson;
+        }
+    }
+}])
