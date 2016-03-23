61f32869688ce6bd6055ac3e000be8f62a965436
woxieao <xieao@huiber.cn>
2016-03-23 21:51:31 +0800

    分页JS封装

---
+++ src/Giqci.PublicWeb/Scripts/giqci.js
@@ -0,0 +1,73 @@
+if (typeof jQuery === 'undefined') {
+    throw new Error("Giqci's JavaScript requires jQuery");
+}
+
+function PageHandler(postUrl, callBackFunc, customPageSize) {
+    var url = postUrl;
+    var func = callBackFunc;
+    var pageSize = customPageSize == null ? 10 : customPageSize;
+    var pageIndex = 1;
+    var lastTimeResultCount = pageSize;
+
+    function canWeGo(currentPageIndex, type) {
+        var isEnabled;
+        switch (type) {
+        case -1:
+        {
+            isEnabled = currentPageIndex - 1 > 0;
+            if (isEnabled) {
+                --pageIndex;
+            }
+            return isEnabled;
+        }
+        case 1:
+        {
+            isEnabled = lastTimeResultCount === pageSize;
+            if (isEnabled) {
+                ++pageIndex;
+            }
+            return isEnabled;
+        }
+        default:
+        {
+            pageIndex = 1;
+            return false;
+        }
+        }
+    }
+
+    function go(postData) {
+        postData = postData == null ? {} : postData;
+        postData["pageIndex"] = pageIndex;
+        postData["pageSize"] = pageSize;
+        $.ajax({
+            url: url,
+            data: postData,
+            type: "POST",
+            success: function(result) {
+                lastTimeResultCount = result.data.length;
+                if (result.msg != null) {
+                    alert(result.msg);
+                }
+                func(result);
+            },
+            error: function(result) {
+                console.log(result);
+                alert(":(\nWhoops,looks like something went wrong");
+            }
+        });
+    }
+
+    this.FirstPage = function(postData) {
+        pageIndex = 1;
+        go(postData);
+    }
+    this.PrevPage = function(postData) {
+        if (canWeGo(pageIndex, -1))
+            go(postData);
+    }
+    this.NextPage = function(postData) {
+        if (canWeGo(pageIndex, 1))
+            go(postData);
+    }
+}
