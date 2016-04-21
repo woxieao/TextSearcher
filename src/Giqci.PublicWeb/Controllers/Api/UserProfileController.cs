e92e00ec6949d325f4eff3221774cc6ea0d01367
woxieao <xieao@huiber.cn>
2016-04-21 16:39:14 +0800

    add userprofile controller

---
+++ src/Giqci.PublicWeb/Controllers/Api/UserProfileController.cs
@@ -0,0 +1,65 @@
+using System;
+using Ktech.Mvc.ActionResults;
+using System.Net;
+using System.Web.Mvc;
+using Giqci.Chapi.Enums.App;
+using Giqci.Chapi.Models.App;
+using Giqci.Interfaces;
+using Giqci.PublicWeb.Services;
+
+namespace Giqci.PublicWeb.Controllers.Api
+{
+    [RoutePrefix("api/UserProfile")]
+    [Authorize]
+    public class UserProfileController : Controller
+    {
+        private readonly IAuthService _auth;
+        private readonly IUserProfileApiProxy _userProfileApiProxy;
+
+        public UserProfileController(IAuthService auth, IUserProfileApiProxy userProfileApiProxy)
+        {
+            _auth = auth;
+            _userProfileApiProxy = userProfileApiProxy;
+        }
+
+        [Route("GetUserProfileList")]
+        [HttpPost]
+        public ActionResult GetUserProfileList(UserType? userType)
+        {
+            var profileList = _userProfileApiProxy.Select(_auth.GetAuth().MerchantId, userType);
+            return new KtechJsonResult(HttpStatusCode.OK, new {result = profileList});
+        }
+
+        [Route("UserAddProfile")]
+        [HttpPost]
+        public ActionResult UserAddProfile(UserProfile userProfile)
+        {
+            bool flag = true;
+            try
+            {
+                _userProfileApiProxy.Add(_auth.GetAuth().MerchantId, userProfile);
+            }
+            catch
+            {
+                flag = false;
+            }
+            return new KtechJsonResult(HttpStatusCode.OK, new {flag = flag});
+        }
+
+        [Route("RemoveProfile")]
+        [HttpPost]
+        public ActionResult RemoveProfile(int profileId)
+        {
+            var flag = true;
+            try
+            {
+                _userProfileApiProxy.Remove(_auth.GetAuth().MerchantId, profileId);
+            }
+            catch
+            {
+                flag = false;
+            }
+            return new KtechJsonResult(HttpStatusCode.OK, new {flag = flag});
+        }
+    }
+}
