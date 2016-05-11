301ce3987e4d33d384f456cd6073bac5d3511b58
hulj <hulj@huiber.cn>
2016-05-11 14:18:57 +0800

    添加国家默认值

--- src/Giqci.PublicWeb/Controllers/FormsController.cs
+++ src/Giqci.PublicWeb/Controllers/FormsController.cs
@@ -61,6 +61,7 @@ namespace Giqci.PublicWeb.Controllers
                     new ApplicationProduct
                     {
                         HandlerType = HandlerType.Add,
+                        ManufacturerCountry = "036",
                         ProductItemList = new List<ProductItem>
                         {
                             new ProductItem
@@ -91,18 +92,18 @@ namespace Giqci.PublicWeb.Controllers
             switch (application.Status)
             {
                 case ApplicationStatus.New:
-                {
-                    return View(application);
-                }
+                    {
+                        return View(application);
+                    }
                 default:
-                {
-                    var certs = _certRepo.Select(appkey);
-                    var validCerts = certs.GroupBy(i => i.CertType);
-                    ViewBag.ValidCerts =
-                        validCerts.Select(certTypeList => certTypeList.OrderByDescending(i => i.SignedDate).First())
-                            .ToList();
-                    return View("ViewApp", application);
-                }
+                    {
+                        var certs = _certRepo.Select(appkey);
+                        var validCerts = certs.GroupBy(i => i.CertType);
+                        ViewBag.ValidCerts =
+                            validCerts.Select(certTypeList => certTypeList.OrderByDescending(i => i.SignedDate).First())
+                                .ToList();
+                        return View("ViewApp", application);
+                    }
             }
         }
 
@@ -151,7 +152,7 @@ namespace Giqci.PublicWeb.Controllers
             if (string.IsNullOrEmpty(userName))
             {
                 isLogin = false;
-                errors = new List<string>() {"登录状态已失效，请您重新登录系统"};
+                errors = new List<string>() { "登录状态已失效，请您重新登录系统" };
             }
             var merchantId = _merchantRepo.GetMerchant(User.Identity.Name).Id;
 
@@ -169,7 +170,7 @@ namespace Giqci.PublicWeb.Controllers
                 errors = null;
             }
             return new KtechJsonResult(HttpStatusCode.OK,
-                new {isNew = isNew, appkey = appkey, isLogin = isLogin, errors = errors});
+                new { isNew = isNew, appkey = appkey, isLogin = isLogin, errors = errors });
         }
 
 
