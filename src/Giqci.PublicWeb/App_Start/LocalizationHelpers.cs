be61ab297861b0a951ba51f9384fba9cdd090bee
hulj <hulj@huiber.cn>
2016-07-07 09:46:19 +0800

    add HtmlHelper

---
+++ src/Giqci.PublicWeb/App_Start/LocalizationHelpers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Giqci.ApiProxy.Logging;
+using Giqci.ApiProxy.Services;
+using Giqci.Chapi.Enums.Dict;
+using Giqci.Chapi.Models.Logging;
+using Giqci.Interfaces;
+using Giqci.PublicWeb.Models;
+
+namespace Giqci.PublicWeb
+{
+    public static class LocalizationHelpers
+    {
+        private static readonly IDictService _dict;
+
+        static LocalizationHelpers()
+        {
+            var httpclient = RestClientFactory.Create(Config.ApiUrl.DictApiUrl, false);
+            var log = new LoggingApiProxy(new LogConfig()
+            {
+                Log = Config.LogSwitch.DictLogSwitch,
+                LogUrl = Config.ApiUrl.DictApiUrl
+            });
+            _dict = new CachedDictService(httpclient, log);
+        }
+
+        public static string Language(this HtmlHelper helper, string key)
+        {
+            LanguageType language = Config.Common.Language;
+            return _dict.GetLanguage(language, key);
+        }
+    }
+}
