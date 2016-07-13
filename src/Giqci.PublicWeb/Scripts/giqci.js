d314641cb6ad54f36802e49b0eeca58c986fa4be
woxieao <woxieao@huiber.cn>
2016-07-13 16:21:33 +0800

    多语言 设置键值

--- src/Giqci.PublicWeb/Scripts/giqci.js
+++ src/Giqci.PublicWeb/Scripts/giqci.js
@@ -316,7 +316,6 @@ $giqci.getLanUrl = function (url) {
         return lan;
     }
 }
-
 $giqci.getLanType = function () {
     return window.location.pathname.split('/')[1].toLocaleLowerCase();
 }
@@ -324,12 +323,16 @@ $giqci.getLanType = function () {
 $giqci.KeyToWord = function (keyName) {
     var lanType = $giqci.getLanType();
     var key = LanguageData[keyName];
-    switch (lanType) {
-        case "cn":
-            return key === undefined || key === null ? keyName.replace(/_/g, " ") : key.CnName;
-        case "en":
-            return key === undefined || key === null ? keyName.replace(/_/g, " ") : key.EnName;
-        default:
-            return "UnknownLanguageType";
+    if (key == null) {
+        return keyName.replace(/_/g, " ");
+    } else {
+        switch (lanType) {
+            case "cn":
+                return key.CnName == null ? keyName.replace(/_/g, " ") : key.CnName;
+            case "en":
+                return key.EnName == null ? keyName.replace(/_/g, " ") : key.EnName;
+            default:
+                return "UnknownLanguageType";
+        }
     }
 }
